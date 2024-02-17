e2a673bba30f7ba10b7e8c278539fb11bed2a3e7
Subodh Kshirsagar Kamble <kamblesubodh189@gmail.com>
2024-02-17 15:53:43 +0530

    complted the useReducer hook

--- native-useState/src/App.jsx
+++ native-useState/src/App.jsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+function NameList() {
+  const [list, setList] = useState(["subodh", "sumedh", "sanjivanne"]);
+  const [name, setName] = useState(() => "subodh");
+
+  const onAddName = () => {
+    setList([...list, name]);
+    setName("");
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <ul>
+        {list.map((name) => (
+          <li key={name}>{name}</li>
+        ))}
+      </ul>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <button onClick={onAddName}>Add Name</button>
+    </div>
+  );
 }
 
-export default App
+function Counter() {
+  const [count, setCount] = useState(10);
+
+  function addOne() {
+    setCount(count + 1);
+  }
+
+  return (
+    <div className="App">
+      <button onClick={addOne}>Count={count}</button>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <Counter />
+      <NameList />
+    </div>
+  );
+}
+
+export default App;
