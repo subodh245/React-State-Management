d7985f68d46caed7d4ec2d45a8129b42aabc26a9
Subodh Kshirsagar Kamble <kamblesubodh189@gmail.com>
2024-02-19 06:16:59 +0530

    completed useCallback

--- useMemo-useCallback/src/App.jsx
+++ useMemo-useCallback/src/App.jsx
@@ -1,4 +1,15 @@
+import { useCallback } from "react";
 import { useMemo, useState } from "react";
+
+function SortedList({ list, sortFunc }) {
+  console.log("SortedList render");
+
+  const sortedList = useMemo(() => {
+    console.log("running sort");
+    return [...list].sort(sortFunc);
+  }, [list, sortFunc]);
+  return <div>{sortedList.join(", ")}</div>;
+}
 
 function App() {
   const [numbers] = useState([10, 20, 38]);
@@ -16,11 +27,13 @@ function App() {
 
   const countTotal = count1 + count2; //useMemo(() => count1 + count2, [count1,count2])
 
+  const sortFunc = useCallback((a, b) => a.localeCompare(b), []);
+
   return (
     <>
       <div>Total:{total}</div>
       <div>Name:{names.join(", ")}</div>
-      <div>SortedName: {sortedNames.join(", ")}</div>
+      <SortedList list={names} sortFunc={sortFunc} />
       <button onClick={() => setCount1(count1 + 1)}>Count: {count1}</button>
       <button onClick={() => setCount2(count2 + 2)}>Count: {count2}</button>
       <div>Total:{countTotal}</div>
