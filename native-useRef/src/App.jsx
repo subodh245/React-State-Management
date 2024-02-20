7a9aa3fa20c8a95cd332dcd4b31ed956ade0e750
Subodh Kshirsagar Kamble <kamblesubodh189@gmail.com>
2024-02-20 13:18:21 +0530

    completed useReff hook

---
+++ native-useRef/src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import { useRef } from "react";
+function App() {
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
+  const idRef = useRef(1);
+  const [names, setNames] = useState([
+    { id: idRef.current++, name: "shradha kapoooor" },
+    { id: idRef.current++, name: "rashmika manadananana" },
+  ]);
+
+  const onAddName = () => {
+    setNames([
+      ...names,
+      {
+        id: idRef.current++,
+        name: inputRef.current.value,
+      },
+    ]);
+    inputRef.current.value = "";
+  };
+
+  return (
+    <div>
+      <div>
+        {names.map((name) => (
+          <div key={name.name}>
+            {name.id}-{name.name}
+          </div>
+        ))}
+      </div>
+      <input type="text" ref={inputRef} />
+      <button onClick={onAddName}>Add Name</button>
+    </div>
+  );
+}
+
+export default App;
