d1a891bce106b958d893a5a354aec74081ba4509
Subodh Kshirsagar Kamble <kamblesubodh189@gmail.com>
2024-02-23 08:11:13 +0530

    .

---
+++ direct-pokemon-jotai/src/store.tsx
@@ -0,0 +1,33 @@
+import { atom } from "jotai";
+import { atomsWithQuery } from "jotai-tanstack-query";
+
+export interface Pokemon {
+  id: number;
+  name: string;
+  type: string[];
+  hp: number;
+  attack: number;
+  defense: number;
+  special_attack: number;
+  special_defense: number;
+  speed: number;
+}
+
+export const searchAtom = atom("");
+
+const [allPokemon] = atomsWithQuery<Pokemon[]>(() => ({
+  queryKey: ["pokemon"],
+  queryFn: () => fetch("/pokemon.json").then((res) => res.json()),
+}));
+
+export const pokemonAtom = atom((get) => {
+  const search = get(searchAtom).toLowerCase();
+  const all: Pokemon[] | undefined = get(allPokemon);
+  if (!all) return [];
+  return all.filter((p: Pokemon) => p.name.toLowerCase().includes(search));
+});
+
+export const sortedPokemonAtom = atom((get) => {
+  const pokemon = get(pokemonAtom);
+  return pokemon.slice(0, 10).sort((a, b) => a.name.localeCompare(b.name));
+});
---
+++ redux/src/store.tsx
@@ -0,0 +1,50 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import {
+  createSlice,
+  configureStore,
+  type PayloadAction,
+} from "@reduxjs/toolkit";
+export interface Pokemon {
+  id: number;
+  name: string;
+  type: string[];
+  hp: number;
+  attack: number;
+  defense: number;
+  special_attack: number;
+  special_defense: number;
+  speed: number;
+}
+
+const pokemonApi = createApi({
+  reducerPath: "pokemonApi",
+  baseQuery: fetchBaseQuery({ baseUrl: "/" }),
+  endpoints: (builder) => ({
+    getPokemon: builder.query<Pokemon[], undefined>({
+      query: () => "pokemon.json",
+    }),
+  }),
+});
+const pokemonApi.endpoints.getPokemon.useQuery;
+
+const searchSlice = createSlice({
+  name: "search",
+  initialState: {
+    search: "",
+  },
+  reducers: {
+    setSearch: (state, action: PayloadAction<string>) => {
+      state.search = action.payload;
+    },
+  },
+});
+
+export const { setSearch } = searchSlice.actions;
+
+export const store = configureStore({
+  reducer: {
+    search: searchSlice.reducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
